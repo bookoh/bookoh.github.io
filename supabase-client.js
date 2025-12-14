a035357ca49894e061ec3f75c007831b37350ad0
bookoh <maple3589@gmail.com>
2025-12-14 09:11:18 +0900

    Update supabase-client.js

--- supabase-client.js
+++ supabase-client.js
@@ -3,5 +3,7 @@
 <script>
   const SUPABASE_URL = 'https://dmjaxhydwaumhujrculd.supabase.co';
   const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRtamF4aHlkd2F1bWh1anJjdWxkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ3MTEyNTgsImV4cCI6MjA4MDI4NzI1OH0.nTdfW9Iw3OLyYbWcPGU2T8P_i-9tDeqUn1t2cAsIbn0';
-  const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+  const { createClient } = window.supabase;   // 네임스페이스에서 함수만 뽑기
+  const supa = createClient(SUPABASE_URL, SUPABASE_ANON_KEY); // 여기만 supa
 </script>
